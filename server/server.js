d93a646f80e1ff694b9b3337f84eefce139932d3
Dennis Gabeler <dc.gabeler@gmail.com>
2017-04-22 01:19:37 +0200

    Added MONGOLAB vars

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
-// var {ObjectID} = require('mongodb');
+var {ObjectID} = require('mongodb');
 
 var express = require('express');
 var bodyParser = require('body-parser');
 
 
-// var {mongoose} = require('./db/mongoose');
-// var {Todo} = require('./models/todo');
-// var {User} = require('./models/user');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -22,46 +22,46 @@ app.get('/', (req,res) => {
     res.send(process.env.MONGODB_URI);
 });
 
-// app.post('/todos', (req, res) => {
-//     var todo = new Todo({
-//         text: req.body.text
-//     });
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
 
-//     todo.save().then((doc) => {
-//         res.send(doc);
-//     }, (e) => {
-//         res.status(400).send(e);
-//     });
-// });
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
 
-// app.get('/todos', (req,res) => {
-//     Todo.find().then((todos) => {
-//         res.send({todos});
-//     }, (e) => {
-//         res.status(400).send(e);
-//     });
-// });
+app.get('/todos', (req,res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
 
-// app.get('/todos/:id', (req, res) => {
-//     var id = req.params.id; 
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id; 
     
-//     if(!ObjectID.isValid(id)) {
-//         return res.status(404).send();
-//     }
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
 
-//     Todo.findById(id).then((todo) => {
-//         if(!todo) {
-//             return res.status(404).send();
+    Todo.findById(id).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
             
-//         }
-//         res.status(200).send({todo});
-//     }, (e) => {
-//         res.status(400).send();
-//     })
-// });
+        }
+        res.status(200).send({todo});
+    }, (e) => {
+        res.status(400).send();
+    })
+});
 
-// app.listen(port, () => {
-//     console.log(`Started up on port ${port}`);
-// });
+app.listen(port, () => {
+    console.log(`Started up on port ${port}`);
+});
 
-// module.exports = {app};
+module.exports = {app};
