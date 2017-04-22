ee8f19c06fa6cfe3f1fa5f32201afc5bd6342491
Dennis Gabeler <dc.gabeler@gmail.com>
2017-04-22 02:13:07 +0200

    Changed MongoDB var

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 //mongoose.connect(process.env.PROD_MONGODB || 'mongodb://localhost:27017/TodoApp');
-mongoose.connect('mongodb://magicboy159:Pluisje20088@ds055925.mlab.com:55925/todoapp');
+mongoose.connect(process.env.PROD_MONGODB || 'mongodb://magicboy159:Pluisje20088@ds055925.mlab.com:55925/todoapp');
 
 module.exports = {mongoose};
